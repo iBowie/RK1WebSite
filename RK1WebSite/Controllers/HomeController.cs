08c0f68ca0944843cca1ba08d1440a1960472623
BowieD <31174978+iBowie@users.noreply.github.com>
2022-03-18 12:18:43 +0500

    end of lesson 8

---
+++ RK1WebSite/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RK1WebSite.Data.Interfaces;
+using RK1WebSite.ViewModels;
+
+namespace RK1WebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAllCars _carRep;
+
+        public HomeController(IAllCars carRep)
+        {
+            _carRep = carRep;
+        }
+
+        public ViewResult Index()
+        {
+            var vm = new HomeViewModel()
+            {
+                FavoriteCars = _carRep.FavoriteCars,
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ RK1WebSite/ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using RK1WebSite.Data.Models;
+
+namespace RK1WebSite.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Car> FavoriteCars { get; init; }
+    }
+}
